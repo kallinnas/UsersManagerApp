501562010074ed7223984dfadad6576b1308d2ee
kallinnas <kallinnas@gmail.com>
2024-11-13 19:37:27 +0200

    fix server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/*', (req, res) => {
 });
 
 // Start the app by listening on the default Railway port
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
