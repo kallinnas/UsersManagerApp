34e44c9f48c41bbde1a481ee62846879e6d61c5a
kallinnas <kallinnas@gmail.com>
2024-11-13 19:33:56 +0200

    +server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve the static files from the Angular dist directory
+app.use(express.static(path.join(__dirname, 'dist/users-manager-app')));
+
+// Redirect all requests to the Angular app
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/users-manager-app/index.html'));
+});
+
+// Start the app by listening on the default Railway port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
